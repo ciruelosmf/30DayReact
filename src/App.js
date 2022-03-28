330e4e61dc4853ed63d5c4afb026e8ec583224d1
ciruelosmf <62398984+ciruelosmf@users.noreply.github.com>
2022-03-28 15:35:03 -0300

    2 day

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import logo from './friedhead.svg';
 import './App.css';
+import ShoutInput from './ShoutInput/ShoutInput';
+import ShoutOutput from './ShoutOutput/ShoutOutput';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+   currentshout: ""
+  }
+
+
+updateShoutHandler = (event) => {
+  this.setState({currentshout: event.target.value})
+  };
+
+
+  render() {
+    return (
+      <div className="App">
+
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>30 days of React</h1>
+          <h2>Day (1+1) / Make to upper case</h2>
+          <h3>You write something in the bar; the text will become uppercased</h3>
+        </header>
+
+
+        <ShoutInput 
+        changed={this.updateShoutHandler}
+        />
+      
+        <ShoutOutput
+        shout={(this.state.currentshout).toUpperCase()}
+        />
+
+      </div>
+    );
+  }
 }
 
 export default App;
