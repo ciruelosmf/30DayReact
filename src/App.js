5c939012b51a6126b4649bbffdc7fe8cc30541b9
matías federico ciruelos <62398984+ciruelosmf@users.noreply.github.com>
2022-04-01 03:18:51 +0000

    D4

--- src/App.js
+++ src/App.js
@@ -1,42 +1,85 @@
 import React, { Component } from 'react';
-import logo from './friedhead.svg';
+import logo from './clock.svg';
 import './App.css';
-import ShoutInput from './ShoutInput/ShoutInput';
-import ShoutOutput from './ShoutOutput/ShoutOutput';
+import FruitFact from './FruitFact/FruitFact';
+
 
 class App extends Component {
-
   state = {
-   currentshout: ""
+    fruits: [
+      { name: 'Banana', fact: 'I am yellow in colour' },
+      { name: 'Peach', fact: 'I have a fuzzy skin' },
+      { name: 'Apple', fact: 'I tend to be crunchy' },
+  ],
+  showFruits: false,
+  showFruitMessage: 'Show Me Fruit Facts',
+}
+
+
+toggleFruitFactsHandler = () => {
+  const currentlyShowing = this.state.showFruits;
+    this.setState({showFruits: !currentlyShowing});
+       
+    if (currentlyShowing === true) {
+      this.setState({showFruitMessage: 'Show Me Fruit Facts'});
+    } else {
+      this.setState({showFruitMessage: 'Fruit Facts Are Boring'});          
+    }
+}
+
+
+render() {
+
+// Here we place the contentwe wish to render conditionally
+  let fruitFacts = null;
+   if (this.state.showFruits) {
+ 
+    fruitFacts =(
+
+        <div className="container">
+        
+        <FruitFact
+        fruit={this.state.fruits[0].name}
+        fact={this.state.fruits[0].fact}
+        />
+
+        <FruitFact
+        fruit={this.state.fruits[1].name}
+        fact={this.state.fruits[1].fact}
+        />
+
+        <FruitFact
+        fruit={this.state.fruits[2].name}
+        fact={this.state.fruits[2].fact}
+        />
+
+        </div>
+    );
   }
 
 
-updateShoutHandler = (event) => {
-  this.setState({currentshout: event.target.value})
-  };
+//The Return Block
 
-
-  render() {
     return (
-      <div className="App">
+    <div className="App">
+  
 
-        <header className="App-header">
+      <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1>30 days of React</h1>
-          <h2>Day (1+1) / Make to upper case</h2>
-          <h3>You write something in the bar; the text will become uppercased</h3>
-        </header>
+          <h2>Day Three / Fruit Fact Revealer</h2>
+      </header>
+    
 
-
-        <ShoutInput 
-        changed={this.updateShoutHandler}
-        />
-      
-        <ShoutOutput
-        shout={(this.state.currentshout).toUpperCase()}
-        />
-
+      <div className="container">
+        <button
+        onClick={this.toggleFruitFactsHandler}>{this.state.showFruitMessage}</button>
       </div>
+
+      {fruitFacts}
+
+    </div>
+
     );
   }
 }
